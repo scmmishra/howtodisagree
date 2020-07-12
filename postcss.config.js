217cb9862c9914115ff2713455fa915673eab014
Shivam Mishra <scm.mymail@gmail.com>
2020-07-12 18:54:33 +0530

    feat: update styling

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     require('autoprefixer'),
     process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
       content: [
-        './public/*.html',
+        './public/index.html',
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
